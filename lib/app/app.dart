a7b7bfc0e68caf0ef5b3895ad844a341750db212
yasmine <iayasmine34@gmail.com>
2025-02-19 00:18:07 +0200

    add  locale 

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
 import '../presentation/resources/app_routes.dart';
 import '../presentation/resources/app_theme.dart';
 
 class ShopeApp extends StatelessWidget {
   const ShopeApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: Locale('ar'),
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       routes: AppRoutes.routes,
       theme: AppTheme.getLightTheme(),
       debugShowCheckedModeBanner: false,
